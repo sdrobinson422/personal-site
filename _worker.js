bb70872a6c14856f168e5a249f72781c5257b36f
Shiloh Robinson <59101177+sdrobinson422@users.noreply.github.com>
2025-12-24 20:57:18 -0800

    no 404

--- _worker.js
+++ _worker.js
@@ -1,23 +1,23 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
+// export default {
+//   async fetch(request, env) {
+//     const url = new URL(request.url);
     
-    // Try to fetch the requested asset
-    let response = await env.ASSETS.fetch(request);
+//     // Try to fetch the requested asset
+//     let response = await env.ASSETS.fetch(request);
     
-    // If we get a 404, serve custom 404 page
-    if (response.status === 404) {
-      const notFoundUrl = url.origin + '404.html';
-      response = await env.ASSETS.fetch(notFoundUrl);
+//     // If we get a 404, serve custom 404 page
+//     if (response.status === 404) {
+//       const notFoundUrl = url.origin + '404.html';
+//       response = await env.ASSETS.fetch(notFoundUrl);
       
-      // Return with 404 status but the HTML content
-      return new Response(response.body, {
-        status: 404,
-        headers: response.headers
-      });
-    }
+//       // Return with 404 status but the HTML content
+//       return new Response(response.body, {
+//         status: 404,
+//         headers: response.headers
+//       });
+//     }
     
-    // Return the successful response as-is
-    return response;
-  }
-};
+//     // Return the successful response as-is
+//     return response;
+//   }
+// }; 
